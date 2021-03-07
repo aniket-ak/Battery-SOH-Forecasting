33f59f3a97475ed3786b2aa166de31ef69e19883
Aniket <aniket.cfd@gmail.com>
2021-03-07 23:10:47 +0530

    initial changes

--- src/gpr/GPR.py
+++ src/gpr/GPR.py
@@ -7,6 +7,11 @@ import argparse
 import logging
 import sys
 
+from matplotlib import pyplot as plt
+import numpy as np
+from scipy.optimize import minimize
+from scipy.spatial.distance import pdist, cdist, squareform
+
 from gpr import __version__
 
 __author__ = "Aniket"
@@ -16,80 +21,32 @@ __license__ = "mit"
 _logger = logging.getLogger(__name__)
 
 
-def fib(n):
-    """Fibonacci example function
+class GPR():
 
-    Args:
-      n (int): integer
+    def __init__(self, kernel, optimizer='L-BFGS-B', noise_var=1e-8):
+        self.kernel = kernel
+        self.noise_var = noise_var
+        self.optimizer = optimizer
 
-    Returns:
-      int: n-th Fibonacci number
-    """
-    assert n > 0
-    a, b = 1, 1
-    for i in range(n - 1):
-        a, b = b, a + b
-    return a
+    # 'Public' methods
+    def sample_prior(self, X_test, n_samples):
+        pass
 
+    def sample_posterior(self, X_test, n_samples):
+        pass
 
-def parse_args(args):
-    """Parse command line parameters
+    def log_marginal_likelihood(self, theta=None, eval_gradient=None):
+        pass
 
-    Args:
-      args ([str]): command line parameters as list of strings
+    def optimize(self, theta, X_train, y_train):
+        pass
 
-    Returns:
-      :obj:`argparse.Namespace`: command line parameters namespace
-    """
-    parser = argparse.ArgumentParser(description="Just a Fibonacci demonstration")
-    parser.add_argument(
-        "--version",
-        action="version",
-        version="GPR {ver}".format(ver=__version__),
-    )
-    parser.add_argument(dest="n", help="n-th Fibonacci number", type=int, metavar="INT")
-    parser.add_argument(
-        "-v",
-        "--verbose",
-        dest="loglevel",
-        help="set loglevel to INFO",
-        action="store_const",
-        const=logging.INFO,
-    )
-    parser.add_argument(
-        "-vv",
-        "--very-verbose",
-        dest="loglevel",
-        help="set loglevel to DEBUG",
-        action="store_const",
-        const=logging.DEBUG,
-    )
-    return parser.parse_args(args)
+    # 'Private' helper methods
+    def _cholesky_factorise(y_cov):
+        pass
 
-
-def setup_logging(loglevel):
-    """Setup basic logging
-
-    Args:
-      loglevel (int): minimum loglevel for emitting messages
-    """
-    logformat = "[%(asctime)s] %(levelname)s:%(name)s:%(message)s"
-    logging.basicConfig(
-        level=loglevel, stream=sys.stdout, format=logformat, datefmt="%Y-%m-%d %H:%M:%S"
-    )
-
-
-def main(args):
-    """Main entry point allowing external calls
-
-    Args:
-      args ([str]): command line parameter list
-    """
-    args = parse_args(args)
-    setup_logging(args.loglevel)
-    _logger.debug("Starting crazy calculations...")
-    print("The {}-th Fibonacci number is {}".format(args.n, fib(args.n)))
-    _logger.info("Script ends here")
+    def _sample_multivariate_gaussian(y_mean, y_cov):
+        pass
 
 
 def run():
